61b7cc5fada68efe63bc604ed290005b864b46bd
Valeria-gthb <lnikolaeva404@gmail.com>
2025-04-01 13:09:34 +0300

    Add files via upload

---
+++ z2repeat.py
@@ -0,0 +1,8 @@
+a = input("Введите первую стадию развития человека (Australopithecus): ")
+b = input("Введите вторую стадию развития человека (Homo habilis): ")
+c = input("Введите третью стадию развития человека (Homo erectus): ")
+d = input("Введите четвертую стадию развития человека (Homo heidelbergensis): ")
+i = input("Введите пятую стадию развития человека (Homo neanderthalensis): ")
+f = input("Введите последнюю стадию развития человека (Homo sapiens sapiens): ")
+
+print(a, b, c, d, i, f, sep=" => ")
